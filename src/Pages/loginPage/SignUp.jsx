b9ce167c83333b87a2b0f755df8cb96e7da0207f
Akanksha-Sanapala <akanksha.sanapal@beautosys.com>
2025-05-08 19:22:25 +0530

    Login page, sign Up Page

--- src/Pages/loginPage/SignUp.jsx
+++ src/Pages/loginPage/SignUp.jsx
@@ -4,21 +4,26 @@ import * as Yup from "yup";
 import { Form, Formik } from "formik";
 import TextFieldComponent from "../../components/form/textFieldComponent";
 
-import LoginPageImage from "../../assets/loginPage.png";
 import InstaLabel from "../../assets/instaLabel.png";
-import FbLogo from "../../assets/fbLogo.png";
+import SignUpFb from "../../assets/signUpFb.png";
+import Gplay from "../../assets/gPlay.png";
+import Microsoft from "../../assets/microsoft.png";
 
 import "../../pages/loginPage/login.css";
 
-function LogIn() {
+function SignUp() {
   const validationSchema = Yup.object({
-    userName: Yup.mixed().required(" is required"),
-    password: Yup.mixed().required("Lead Status is required"),
+    number: Yup.string().required(" is required"),
+    password: Yup.string().required("Lead Status is required"),
+    fullName: Yup.string().required("Lead Status is required"),
+    userName: Yup.string().required("Lead Status is required"),
   });
 
   const initialValues = {
-    userName: null,
-    password: null,
+    number: "",
+    password: "",
+    fullName: "",
+    userName: "",
   };
 
   const onFormSubmit = (values) => {
@@ -27,11 +32,14 @@ function LogIn() {
 
   return (
     <>
-      <div style={{ display: "flex", flexDirection: "column", gap: "60px" }}>
-        <div style={{ height: "100%", display: "flex", gap: "20px" }}>
-          <div>
-            <img style={{ height: "450px" }} src={LoginPageImage} alt="Login" />
-          </div>
+      <div
+        style={{
+          border: "1px solid rgb(219, 219, 219)",
+          height: "679px",
+          width: "347px",
+        }}
+      >
+        <div style={{ display: "flex", flexDirection: "column", gap: "60px" }}>
           <div>
             <main style={{ height: "100%" }}>
               <div
@@ -48,47 +56,29 @@ function LogIn() {
                     src={InstaLabel}
                     alt="Login"
                   />
+                  <div>Sign up to see photos and videos from your friends.</div>
                 </div>
-                <Formik
-                  initialValues={initialValues}
-                  validationSchema={validationSchema}
-                  onSubmit={(values) => {
-                    onFormSubmit(values);
+
+                {/* Login with facebook */}
+                <div
+                  className="login-fb-btn"
+                  style={{
+                    display: "flex",
+                    gap: "7px",
                   }}
                 >
-                  {({ values, setFieldValue, touched, errors }) => (
-                    <Form style={{ height: "100%" }}>
-                      <div>
-                        <div>
-                          <TextFieldComponent
-                            label="Phone number, username, or email"
-                            name="userName"
-                          />
-                        </div>
-                        <div>
-                          <TextFieldComponent
-                            label="Password"
-                            name="password"
-                          />
-                        </div>
+                  <img
+                    style={{
+                      height: "16px",
+                      width: "16px",
+                    }}
+                    src={SignUpFb}
+                  />
 
-                        {/* Login button  */}
-
-                        <div
-                          className="login-button fontStyles"
-                          style={{
-                            height: "30px",
-                            color: "white",
-                            fontWeight: "500",
-                            width: "100%",
-                          }}
-                        >
-                          Log in
-                        </div>
-                      </div>
-                    </Form>
-                  )}
-                </Formik>
+                  <div className="login-fb " style={{ color: "white" }}>
+                    Log in with Facebook
+                  </div>
+                </div>
                 {/* or with borders */}
                 <div style={{ display: "flex" }}>
                   <div
@@ -136,18 +126,56 @@ function LogIn() {
                   </div>
                 </div>
 
-                {/* Login with facebook */}
-                <div
-                  style={{
-                    display: "flex",
-                    gap: "7px",
-                    margin: "15px 30px",
+                <Formik
+                  initialValues={initialValues}
+                  validationSchema={validationSchema}
+                  onSubmit={(values) => {
+                    onFormSubmit(values);
                   }}
                 >
-                  <img style={{ height: "20px", width: "20px" }} src={FbLogo} />
+                  {({ values, setFieldValue, touched, errors }) => (
+                    <Form style={{ height: "100%" }}>
+                      <div>
+                        <div>
+                          <TextFieldComponent
+                            label="Mobile Number or Email"
+                            name="number"
+                          />
+                        </div>
+                        <div>
+                          <TextFieldComponent
+                            label="Password"
+                            name="password"
+                          />
+                        </div>
+                        <div>
+                          <TextFieldComponent
+                            label="Full Name"
+                            name="fullName"
+                          />
+                        </div>
+                        <div>
+                          <TextFieldComponent
+                            label="Username"
+                            name="userName"
+                          />
+                        </div>
+                      </div>
 
-                  <div className="login-fb ">Log in with Facebook</div>
-                </div>
+                      {/* Login button  */}
+
+                      <div
+                        className="login-button fontStyles"
+                        style={{
+                          color: "white",
+                          fontWeight: "500",
+                        }}
+                      >
+                        Sign up
+                      </div>
+                    </Form>
+                  )}
+                </Formik>
 
                 {/* Forget Password  */}
                 <div
@@ -169,8 +197,45 @@ function LogIn() {
           </div>
         </div>
 
+        {/* Log in  */}
+        <div
+          style={{
+            border: "1px solid rgb(219, 219, 219)",
+            marginTop: "22px",
+            padding: "15px",
+          }}
+        >
+          <div>Have an account?</div>
+          <div>Log in</div>
+        </div>
+
+        {/* Get the app  */}
+        <div style={{ marginTop: "15px" }}>
+          <div>Get the app.</div>
+          <div
+            style={{
+              display: "flex",
+              gap: "10px",
+              marginTop: "15px",
+              marginLeft: "25px",
+            }}
+          >
+            <img style={{ height: "40px", width: "134px" }} src={Gplay} />
+
+            <img style={{ height: "40px", width: "134px" }} src={Microsoft} />
+          </div>
+        </div>
+
         {/* Links at last  */}
-        <div style={{ display: "flex", gap: "15px" }}>
+        <div
+          style={{
+            display: "flex",
+            gap: "65px",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "50%",
+          }}
+        >
           <a
             href="https://www.meta.com/about/?utm_source=about.meta.com&utm_medium=redirect"
             target="_blank"
@@ -213,9 +278,10 @@ function LogIn() {
             <span className="links-atlast">Locations</span>
           </a>
 
-          <a href="https://www.instagram.com/web/lite/" target="_self">
-            <span className="links-atlast">Instagram Lite</span>
-          </a>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <a href="https://www.instagram.com/web/lite/" target="_self"></a>
+            <div className="links-atlast">Instagram Lite</div>
+          </div>
 
           <a href="https://developers.facebook.com/docs/instagram-platform">
             <span className="links-atlast">Threads</span>
@@ -229,16 +295,10 @@ function LogIn() {
             <span className="links-atlast">Meta Verified</span>
           </a>
         </div>
-
         <div
           style={{
             display: "flex",
             flexDirection: "row",
-            gap: "15px",
-            alignItems: "center",
-            justifyContent: "center",
-            position: "relative",
-            bottom: "45px",
           }}
         >
           <div className="language">English</div>
@@ -249,4 +309,4 @@ function LogIn() {
   );
 }
 
-export default LogIn;
+export default SignUp;
--- src/components/form/textFieldComponent/index.jsx
+++ src/components/form/textFieldComponent/index.jsx
@@ -5,14 +5,12 @@ import TextField from "@mui/material/TextField";
 export default function TextFieldComponent() {
   return (
     <Box
-      component="form"
+      // component="form"
       sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}
       noValidate
       autoComplete="off"
     >
       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="filled-basic" label="Filled" variant="filled" />
-      <TextField id="standard-basic" label="Standard" variant="standard" />
     </Box>
   );
 }
